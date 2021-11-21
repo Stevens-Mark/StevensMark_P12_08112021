82f651bc1b07489357eb2f35f27b370d4db759f8
Mark Stevens <markspstevens@yahoo.co.uk>
2021-11-21 08:21:04 +0100

    rearrange components

---
+++ src/utils/HelperFunctions/Formatters.jsx
@@ -0,0 +1,44 @@
+// Functions used to format the labels on the different graphs
+
+/**
+ * Format date on X axis from yyyy-mm-dd to dd/mm
+ * on the Daily activities Line graph
+ * @function TranformDate
+ * @param {string} tickItem
+ * @returns {string} formatted Date
+ */
+ const TranformDate = (tickItem) => {
+  let formattedDate = '';
+
+  if (tickItem) {
+    let parts = tickItem.split('-')
+    formattedDate = `${parts[2]}/${parts[1]}`
+  }
+  return formattedDate
+}
+
+/**
+ * Format day on X axis from number to letter
+ * on the Average sessions Line graph
+ * @function TranformDay
+ * @param {number} tickItem
+ * @returns {string} A Day letter
+ */
+ const TranformDay = (tickItem) => {
+  const Day = [ 'L', 'M', 'M', 'J', 'V', 'S', 'D']
+  if (tickItem) return Day[tickItem-1]
+}
+
+/**
+ * Format the labels on the radar axis from number to words
+ * on the Performance Radar chart
+ * @function TranformKind
+ * @param {number} tickItem
+ * @returns {string} Kind: one of the categories.
+ */
+ const TranformKind = (tickItem) => {
+  const Kind = [ 'Intensité', 'Vitesse', 'Force', 'Endurance', 'Energie', 'Cardio']
+  if (tickItem) return Kind[tickItem-1]
+}
+
+export { TranformDate, TranformDay, TranformKind }
